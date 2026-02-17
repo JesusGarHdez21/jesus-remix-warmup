2b997a07b3fad0546b9e399bb2573451700a332d
JesusGarHdez21 <jesusgarciahdez21@gmail.com>
2026-02-17 15:54:33 -0600

    Persistance state

--- app/store/useFavoriteStore.ts
+++ app/store/useFavoriteStore.ts
@@ -1,15 +1,22 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import type { Movie } from '~/types/movie';
 
-export const useFavorite = create((set) => ({
-    favorites: [],
-    toggleFavorite: (movie: Movie) => set((state: any) => {
-        const isFavorite = state.favorites.find((m: any) => m.id === movie.id);
-
-        return {
-            favorites: isFavorite ? state.favorites.filter((m: any) => m.id !== movie.id) : [...state.favorites, movie]
+export const useFavorite = create<FavoriteState>()(
+    persist(
+        (set) => ({
+            favorites: [],
+            toggleFavorite: (movie) =>
+                set((state) => {
+                    const isFavorite = state.favorites.some((f) => f.id === movie.id);
+                    if (isFavorite) {
+                        return { favorites: state.favorites.filter((f) => f.id !== movie.id) };
+                    }
+                    return { favorites: [...state.favorites, movie] };
+                }),
+        }),
+        {
+            name: "movie-favorites"
         }
-    }
     )
-})
 );
