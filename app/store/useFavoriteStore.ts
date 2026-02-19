ccb79c4d9b2ac8a4cea2c62a791ef54eb2ef5fa1
JesusGarciaHdez21 <jesusgarciahdez21@gmail.com>
2026-02-19 08:17:25 -0600

    Added some interfaces to the favoriteStore hook

--- app/store/useFavoriteStore.ts
+++ app/store/useFavoriteStore.ts
@@ -2,15 +2,20 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import type { Movie } from '~/types/movie';
 
+interface FavoriteState {
+    favorites: Movie[];
+    toggleFavorite: (movie: Movie) => void;
+}
+
 export const useFavorite = create<FavoriteState>()(
     persist(
         (set) => ({
             favorites: [],
             toggleFavorite: (movie: Movie) =>
                 set((state: any) => {
-                    const isFavorite = state.favorites.some((f) => f.id === movie.id);
+                    const isFavorite = state.favorites.some((f: Movie) => f.id === movie.id);
                     if (isFavorite) {
-                        return { favorites: state.favorites.filter((f) => f.id !== movie.id) };
+                        return { favorites: state.favorites.filter((f: Movie) => f.id !== movie.id) };
                     }
                     return { favorites: [...state.favorites, movie] };
                 }),
